4b8b6e521b72a7677c745039fb92f0c7a8d5a35d
peterkilungya1@gmail.com <peterkilungya1@gmail.com>
2023-03-22 11:46:25 +0300

    final commit

---
+++ App.js
@@ -0,0 +1,14 @@
+const buyButtons = document.querySelectorAll('.buy');
+const sellButtons = document.querySelectorAll('.sell');
+
+buyButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    alert('You bought a stock!');
+  });
+});
+
+sellButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    alert('You sold a stock!');
+  });
+});
